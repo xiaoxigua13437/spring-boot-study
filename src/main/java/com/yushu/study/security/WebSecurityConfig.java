1536cb4049414e0a611afc1456aef1ed7aab820d
yushu <591821761@qq.com>
2021-01-14 10:32:52 +0800

    开启权限控制

--- src/main/java/com/yushu/study/security/WebSecurityConfig.java
+++ src/main/java/com/yushu/study/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.yushu.learn.security;
+package com.yushu.study.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
